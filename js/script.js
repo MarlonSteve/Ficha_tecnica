a3f82e246f86542ded38ac6b24f7731c45783b9e
Marlonsteve <luisamariasancheztovar2006@gmail.com>
2023-04-17 13:42:55 -0500

    inicializando repositorio

---
+++ js/script.js
@@ -0,0 +1,85 @@
+alert("Especificaciones hardware y software")
+function ficha() {
+    let nompro, linpro, verant
+
+    nompro=document.getElementById("nompro").value
+    document.getElementById("respuesta").innerHTML="<h2>Nombre Del Producto: "+nompro+"</h2>"
+    
+    linpro=document.getElementById("linpro").value
+    document.getElementById("respuesta2").innerHTML="<h2>Linea De Producción: "+linpro+"</h2>"
+   
+    verant=document.getElementById("verant").value
+    document.getElementById("respuesta3").innerHTML="<h2>Versiones Anteriores: "+verant+"</h2>"
+
+    verson=document.getElementById("verson").value
+    document.getElementById("respuesta4").innerHTML="<h2>Version Actual: "+verson+"</h2>"
+
+   modu=document.getElementById("modu").value
+    document.getElementById("respuesta5").innerHTML="<h2>Modulo: "+modu+"</h2>"
+
+    desc=document.getElementById("desc").value
+    document.getElementById("respuesta6").innerHTML="<h2>Descripcion general del producto: "+desc+"</h2>"
+
+    ob=document.getElementById("ob").value
+    document.getElementById("respuesta7").innerHTML="<h2>Objetivos especificos: "+ob+"</h2>"
+
+    ge=document.getElementById("ge").value
+    document.getElementById("respuesta8").innerHTML="<h2>Arquitectura: "+ge+"</h2>"
+
+    re=document.getElementById("re").value
+    document.getElementById("respuesta9").innerHTML="<h2>Requerimientos funcionales generales: "+re+"</h2>"
+
+    cli=document.getElementById("cli").value
+    document.getElementById("respuesta10").innerHTML="<h2>Clientes del producto: "+cli+"</h2>"
+
+    har=document.getElementById("har").value
+    document.getElementById("respuesta11").innerHTML="<h2>Hardware: "+har+"</h2>"
+
+    sof=document.getElementById("sof").value
+    document.getElementById("respuesta12").innerHTML="<h2>Software: "+sof+"</h2>"
+
+    ot=document.getElementById("ot").value
+    document.getElementById("respuesta13").innerHTML="<h2>Otros: "+ot+"</h2>"
+
+    ree=document.getElementById("ree").value
+    document.getElementById("respuesta14").innerHTML="<h2>Hardware: "+ree+"</h2>"
+
+    ft=document.getElementById("ft").value
+    document.getElementById("respuesta15").innerHTML="<h2>Software: "+ft+"</h2>"
+
+    ros=document.getElementById("ros").value
+    document.getElementById("respuesta16").innerHTML="<h2>Otros: "+ros+"</h2>"
+
+    fecha=document.getElementById("fecha").value
+    document.getElementById("respuesta17").innerHTML="<h2>Version: "+fecha+"</h2>"
+
+    de=document.getElementById("de").value
+    document.getElementById("respuesta18").innerHTML="<h2>Naturaleza de cambios: "+de+"</h2>"
+
+    los=document.getElementById("los").value
+    document.getElementById("respuesta19").innerHTML="<h2>Fecha de aprobacion: "+los+"</h2>"
+
+    tr=document.getElementById("tr").value
+    document.getElementById("respuesta20").innerHTML="<h2>Fecha validacion: "+tr+"</h2>"
+
+    ela=document.getElementById("ela").value
+    document.getElementById("respuesta21").innerHTML="<h2>Elaboro: "+ela+"</h2>"
+
+    apro=document.getElementById("apro").value
+    document.getElementById("respuesta22").innerHTML="<h2>Aprobo: "+apro+"</h2>"
+
+    vali=document.getElementById("vali").value
+    document.getElementById("respuesta23").innerHTML="<h2>Valido: "+vali+"</h2>"
+
+    lu=document.getElementById("lu").value
+    document.getElementById("respuesta24").innerHTML="<h2>date: "+lu+"</h2>"
+
+    mar=document.getElementById("mar").value
+    document.getElementById("respuesta25").innerHTML="<h2>Fecha: "+mar+"</h2>"
+
+    mi=document.getElementById("mi").value
+    document.getElementById("respuesta26").innerHTML="<h2>Fecha2: "+mi+"</h2>"
+}
+
+
+    
